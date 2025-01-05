405a97dd4cacbaa34fe3a368cbf1d34a327acd2f
Satyam Raj <rajsatyammm@gmail.com>
2025-01-06 00:15:56 +0530

    ModelMapper Bean added

--- src/main/java/com/satyam/BlogProjectApplication.java
+++ src/main/java/com/satyam/BlogProjectApplication.java
@@ -1,12 +1,19 @@
 package com.satyam;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogProjectApplication.class, args);
+	}
+
+	@Bean
+	public static ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 }
