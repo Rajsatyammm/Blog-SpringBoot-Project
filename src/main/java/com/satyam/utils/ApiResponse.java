ddb3143857025e36396aecc01a75267f2b4acc58
Satyam Raj <rajsatyammm@gmail.com>
2025-01-06 00:20:52 +0530

    ApiResponse created.

---
+++ src/main/java/com/satyam/utils/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.satyam.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private String message;
+    private Integer statusCode;
+    private Boolean success;
+}
