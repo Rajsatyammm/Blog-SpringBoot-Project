0044c9e5d6f1173e5e59a19a7641453a1cb6a6cd
Satyam Raj <rajsatyammm@gmail.com>
2025-01-06 23:01:33 +0530

    File upload functionality added

---
+++ src/main/java/com/satyam/service/IFileService.java
@@ -0,0 +1,9 @@
+package com.satyam.service;
+
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IFileService {
+    String uploadImage(String path, MultipartFile multipartFile) throws IOException;
+}
