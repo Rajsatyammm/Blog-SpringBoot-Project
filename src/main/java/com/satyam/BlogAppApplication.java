69f69ea7d27d06b2f1fdc5c244f302a22b70d3d8
Satyam Raj <rajsatyammm@gmail.com>
2025-01-18 18:50:18 +0530

    minor change

--- src/main/java/com/satyam/BlogAppApplication.java
+++ src/main/java/com/satyam/BlogAppApplication.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BlogAppApplication {
@@ -15,6 +16,11 @@ public class BlogAppApplication {
 	@Bean
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/satyam/model/Role.java
+++ src/main/java/com/satyam/model/Role.java
@@ -2,6 +2,7 @@ package com.satyam.model;
 
 import java.util.Set;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Role {
 
     @Id
